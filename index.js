ea8db651b2b54509d18d917c6c5b91039f8d298d
jmcavaddy <jakemcavaddy@gmail.com>
2023-03-21 14:32:23 -0400

    Added files, moved initializing function to cli.js, created response chain to handle input and rendering the svg text

--- index.js
+++ index.js
@@ -1,42 +1,5 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const shapes = require('./lib/shapes.js');
+const CLI = require('./lib/cli.js');
 
-// An array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        name: 'text',
-        message: 'What would you like your logo to say?'
-    },
-    {
-        type: 'input',
-        name: 'fontColor',
-        message: 'What color would you like the font of the logo to be?'
-    },
-    {
-        type: 'list',
-        name: 'shape',
-        message: 'What shape would you like your logo to be?',
-        choices: ['circle', 'square', 'triangle']
-    },
-    {
-        type: 'input',
-        name: 'shapeColor',
-        message: 'What would you like the shape of the logo to be?'
-    }
-];
+const cli = new CLI();
 
-
-
-// A function that prompts the user for inputs needed to generate a logo,
-// then passes the user's responses to the generateLogo function
-function init() {
-    inquirer.prompt(questions)
-        .then((response) => {
-            console.log(response);
-        });
-}
-
-// Function call to initialize app
-init();
+cli.init();
